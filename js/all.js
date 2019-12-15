b3fba96f6168a274cf9281f2831807329e8b36a2
sarah <sarah001160@gmail.com>
2019-12-15 14:50:39 +0800

    updatecss

--- js/all.js
+++ js/all.js
@@ -1,12 +1,12 @@
 var map;//使用googleMap api
 function initMap() {//Google Map api
   map = new google.maps.Map(document.querySelector('.map'),{
-    center: {lat:25.015286, lng:121.505052},
-    zoom: 12
+    center: {lat:25.015286, lng:121.505052},//經緯度座標範圍
+    zoom: 12//放大的比例,數字越大距離標點越近;數字越小離標點越遠(就會變成世界地圖)
   });
 
-  var marker = new google.maps.Marker({
-    position:{lat:25.015286, lng:121.505052},
+  var marker = new google.maps.Marker({//紅色標點
+    position:{lat:25.015286, lng:121.505052},//經緯度座標
     map:map,
     title:'公園店'
   })
